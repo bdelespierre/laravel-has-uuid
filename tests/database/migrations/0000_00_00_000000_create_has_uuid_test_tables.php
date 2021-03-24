c06d353b4cc3fd796752c374e42a0350e8e3f06f
Benjamin Delespierre <benjamin.delespierre@gmail.com>
2021-03-24 12:41:29 +0100

    chore: disabling class-declaration psr1 check in migrations

--- tests/database/migrations/0000_00_00_000000_create_has_uuid_test_tables.php
+++ tests/database/migrations/0000_00_00_000000_create_has_uuid_test_tables.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * @phpcs:disable PSR1.Classes.ClassDeclaration
+ */
 class CreateHasUuidTestTables extends Migration
 {
     /**
